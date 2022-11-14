378b997c23b09957abd07e26c278720ac1385eb2
liujiaqi <liujiaqi@e-medclouds.com>
2022-11-14 00:18:46 -0500

    solve bugs about timePicker and room_list.

--- final_project/frontend/site/src/Room/time_picker.js
+++ final_project/frontend/site/src/Room/time_picker.js
@@ -8,8 +8,13 @@ function App({startTime, setStartTime, endTime, setEndTime}) {
 
     const onChange = (value) => {
         // turn to unify timestamp.
-        setStartTime(moment(value[0]).format('YYYY-MM-DD').valueOf())
-        setEndTime(moment(value[1]).format('YYYY-MM-DD').valueOf())
+        if(value != null) {
+            setStartTime(moment(value[0]).format('YYYY-MM-DD').valueOf())
+            setEndTime(moment(value[1]).format('YYYY-MM-DD').valueOf())
+        } else {
+            setStartTime(moment().format('YYYY-MM-DD').valueOf())
+            setEndTime(moment().add(1, "days").format('YYYY-MM-DD').valueOf())
+        }
     }
 
     return (
